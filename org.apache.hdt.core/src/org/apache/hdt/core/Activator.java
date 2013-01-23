c8651b88a1fd8a56f8a2ba173ff12fc79e5f3c1d
adamb <adamb@apache.org>
2013-01-23 11:21:15 -0600

    initial commit of core plugin, that has the mr nature

---
+++ org.apache.hdt.core/src/org/apache/hdt/core/Activator.java
@@ -0,0 +1,31 @@
+package org.apache.hdt.core;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+	public static final String PLUGIN_ID = "org.apache.hdt.core";
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
